f5d70e1f66c96b13ddf8482942b352f0047ea0f8
Nayara <nayfreireof@gmail.com>
2020-01-14 16:58:27 -0300

    Marcando o X

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-function AoClicarX(){
-    document.getElementById('celula11').onclick = function(){
-       
-    }
+function Comeca(){
+// Get the container element
+var sessao = document.getElementById("linha1");
+
+// Get all buttons with class="btn" inside the container
+var celulas1 = sessao.getElementsByClassName("linhas");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < celulas1.length; i++) {
+    celulas1[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("marcado");
+    current[0].className = current[0].className.replace(" marcado", "");
+    this.className += " marcado";
+  });
+}
 }
 
-AoClicarX();
+Comeca();
