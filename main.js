d60e2350f8fd23dcfae2811d7d5215925b36e995
NayFreire <33930743+NayFreire@users.noreply.github.com>
2020-01-17 00:19:07 -0300

    Marcações feitas
    Consegui fazer com que acontecesse a alternação entre os jogadores

--- main.js
+++ main.js
@@ -1,19 +1,251 @@
-function Comeca(){
-// pega o id da sessão
-var sessao = document.getElementById("linha1");
+function marcadoX(event){
+  var player = 1;
+  // var cel = window.document.getElementsByClassName('linhas')
+  // var cel1 = cel[0]
+  // cel1.addEventListener("click", function(){
+  //   cel1.innerHTML = "X"
+  // })
+  var alvo = event.target.getAttribute('id')// pega o id do elemento clicado pelo cursor
+  var marcacoes
+  switch(alvo){
+    case 'celula11':
+      var cel = window.document.getElementById(alvo)
+      cel.innerHTML = "X" //coloca o X na div que foi clicada
+      //cel.style.pointerEvents('none')
+      player = 0;//força a próxima jogada ser feita pela bolinha
+      if (player == 0){
+        document.getElementById('corpo').onclick = marcadoO;// o player 0 é a bolinha, dessa forma, o evento de click do elemento com o id=corpo, vai ser mucada para marcadoO
+      }
 
-// pega todas as divs com a classe "linhas" dentro do section
-var celulas1 = sessao.getElementsByClassName("linhas");
+      /*
+      Marcações
+      X 0 0
+      0 0 0
+      0 0 0
+      
+      */
+      marcacoes[0][0] = 1
 
-// Loop through the buttons and add the active class to the current/clicked button
-// teste teste teste teste
-for (var i = 0; i < celulas1.length; i++) {
-    celulas1[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("marcadoX");
-    current[0].className = current[0].className.replace(" marcadoX", "");
-    this.className += " marcadoX";
-  });
+    break;
+
+    case 'celula12':
+      var cel = window.document.getElementById(alvo)
+      cel.innerHTML = "X"
+
+      player = 0;
+      if (player == 0){
+        document.getElementById('corpo').onclick = marcadoO;
+      }
+
+      marcacoes[0][1] = 1
+    break;
+
+    case 'celula13':
+      var cel = window.document.getElementById(alvo)
+      cel.innerHTML = "X"
+
+      player = 0;
+      if (player == 0){
+        document.getElementById('corpo').onclick = marcadoO;
+      }
+
+      marcacoes[0][2] = 1
+    break;
+
+    case 'celula21':
+      var cel = window.document.getElementById(alvo)
+      cel.innerHTML = "X"
+
+      player = 0;
+      if (player == 0){
+        document.getElementById('corpo').onclick = marcadoO;
+      }
+
+      marcacoes[1][0] = 1
+    break;
+
+    case 'celula22':
+      var cel = window.document.getElementById(alvo)
+      cel.innerHTML = "X"
+
+      player = 0;
+      if (player == 0){
+        document.getElementById('corpo').onclick = marcadoO;
+      }
+
+      marcacoes[1][1] = 1
+    break;
+
+    case 'celula23':
+      var cel = window.document.getElementById(alvo)
+      cel.innerHTML = "X"
+
+      player = 0;
+      if (player == 0){
+        document.getElementById('corpo').onclick = marcadoO;
+      }
+
+      marcacoes[1][2] = 1
+    break;
+
+    case 'celula31':
+      var cel = window.document.getElementById(alvo)
+      cel.innerHTML = "X"
+
+      player = 0;
+      if (player == 0){
+        document.getElementById('corpo').onclick = marcadoO;
+      }
+
+      marcacoes[2][0] = 1
+    break;
+
+    case 'celula32':
+      var cel = window.document.getElementById(alvo)
+      cel.innerHTML = "X"
+
+      player = 0;
+      if (player == 0){
+        document.getElementById('corpo').onclick = marcadoO;
+      }
+
+      marcacoes[2][1] = 1
+    break;
+
+    case 'celula33':
+      var cel = window.document.getElementById(alvo)
+      cel.innerHTML = "X"
+
+      player = 0;
+      if (player == 0){
+        document.getElementById('corpo').onclick = marcadoO;
+      }
+
+      marcacoes[2][2] = 1
+    break;
+  }
 }
+
+marcadoX()
+
+function marcadoO(event){
+  var player = 0;
+  // var cel = window.document.getElementsByClassName('linhas')
+  // var cel1 = cel[0]
+  // cel1.addEventListener("click", function(){
+  //   cel1.innerHTML = "X"
+  // })
+  var alvo = event.target.getAttribute('id')
+
+  switch(alvo){
+    case 'celula11':
+      var cel = window.document.getElementById(alvo)
+      cel.innerHTML = "O"
+
+      player = 1;
+      if (player == 1){
+        document.getElementById('corpo').onclick = marcadoX;
+      }
+
+      marcacoes[0][0] = 0
+    break;
+
+    case 'celula12':
+      var cel = window.document.getElementById(alvo)
+      cel.innerHTML = "O"
+
+      player = 1;
+      if (player == 1){
+        document.getElementById('corpo').onclick = marcadoX;
+      }
+
+      marcacoes[0][1] = 0
+    break;
+
+    case 'celula13':
+      var cel = window.document.getElementById(alvo)
+      cel.innerHTML = "O"
+
+      player = 1;
+      if (player == 1){
+        document.getElementById('corpo').onclick = marcadoX;
+      }
+
+      marcacoes[0][2] = 0
+    break;
+
+    case 'celula21':
+      var cel = window.document.getElementById(alvo)
+      cel.innerHTML = "O"
+
+      player = 1;
+      if (player == 1){
+        document.getElementById('corpo').onclick = marcadoX;
+      }
+
+      marcacoes[1][0] = 0
+    break;
+
+    case 'celula22':
+      var cel = window.document.getElementById(alvo)
+      cel.innerHTML = "O"
+
+      player = 1;
+      if (player == 1){
+        document.getElementById('corpo').onclick = marcadoX;
+      }
+
+      marcacoes[1][1] = 0
+    break;
+
+    case 'celula23':
+      var cel = window.document.getElementById(alvo)
+      cel.innerHTML = "O"
+
+      player = 1;
+      if (player == 1){
+        document.getElementById('corpo').onclick = marcadoX;
+      }
+
+      marcacoes[1][2] = 0
+    break;
+
+    case 'celula31':
+      var cel = window.document.getElementById(alvo)
+      cel.innerHTML = "O"
+
+      player = 1;
+      if (player == 1){
+        document.getElementById('corpo').onclick = marcadoX;
+      }
+
+      marcacoes[2][0] = 0
+    break;
+
+    case 'celula32':
+      var cel = window.document.getElementById(alvo)
+      cel.innerHTML = "O"
+
+      player = 1;
+      if (player == 1){
+        document.getElementById('corpo').onclick = marcadoX;
+      }
+
+      marcacoes[2][1] = 0
+    break;
+
+    case 'celula33':
+      var cel = window.document.getElementById(alvo)
+      cel.innerHTML = "O"
+
+      player = 1;
+      if (player == 1){
+        document.getElementById('corpo').onclick = marcadoX;
+      }
+
+      marcacoes[2][2] = 0
+    break;
+  }
 }
 
-Comeca();
+marcadoO()
